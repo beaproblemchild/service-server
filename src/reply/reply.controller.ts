f09ef4b61a2ec0c039aea32fcf54f189c0811c23
banahaker <pn0818x@outlook.com>
2022-06-22 10:44:25 +0800

    feat: apis for replies

--- src/reply/reply.controller.ts
+++ src/reply/reply.controller.ts
@@ -1,7 +1,23 @@
 import { Controller, Get, Body, Post, Res, Param } from '@nestjs/common';
+import { replyData } from './reply.interface';
 import { ReplyService } from './reply.service';
 
 @Controller('reply')
 export class ReplyController {
   constructor(private readonly ReplyService: ReplyService) {}
+
+  @Get('/:id')
+  getRep(@Param() params) {
+    return this.ReplyService.GetReply(params.id);
+  }
+
+  @Post('/new/:id')
+  addNew(@Param() params, @Body() Body) {
+    const injectData: replyData = {
+      replyTo: params.id,
+      content: Body.content,
+      isAdmin: Body.isAdmin,
+    };
+    return this.ReplyService.AddNewPost(injectData);
+  }
 }
--- src/reply/reply.service.ts
+++ src/reply/reply.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { getRepliesById } from 'src/model/getRepliesById';
+import { addNewReply } from 'src/model/addReply';
+import { replyData } from './reply.interface';
 
 @Injectable()
-export class ReplyService {}
+export class ReplyService {
+  async AddNewPost(injectData: replyData) {
+    try {
+      await addNewReply(injectData);
+      return true;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async GetReply(id: string) {
+    try {
+      const snapshot = await getRepliesById(id);
+      let data: replyData[] = [];
+      snapshot.forEach((doc: replyData | any) => {
+        data.push(doc.data());
+      });
+      return data;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+}
