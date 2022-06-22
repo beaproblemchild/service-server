e97e2065e083c0d1f24141dd4149b37f4fa331c1
banahaker <pn0818x@outlook.com>
2022-06-22 19:14:14 +0800

    hotfix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['localhost:3000', 'beaproblemchild.tk'],
+  });
   await app.listen(8888);
 }
 bootstrap();
