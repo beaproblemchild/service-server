768ea5aac42feef9dec9967fbde38fdf50c3ceb4
banahaker <pn0818x@outlook.com>
2022-06-26 11:37:09 +0800

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://beaproblemchild.tk'],
+    origin: ['http://localhost:3000', 'https://beaproblemchild.tk', "http://192.168.8.129:3000", "https://fascinating-meringue-3c77f1.netlify.app"],
   });
   await app.listen(process.env.PORT || 8888);
 }
